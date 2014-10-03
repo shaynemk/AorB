f9db9df26d7deb266bf7b8001dc73120ea22a5ea
shaynemk <uskickbutt@gmail.com>
2014-10-03 03:36:33 -0400

    Cleaned up a bit of the code, added in FIXME/TODO tags.

--- app/src/main/java/com/keller23/android/aorb/MainActivity.java
+++ app/src/main/java/com/keller23/android/aorb/MainActivity.java
@@ -17,6 +17,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * FIXME Add OnClickListener for the checkboxes to enable the C/D EditBoxes.
+         *        In this OCL, we will listen for the check Mark, then enable the
+         *        corresponding TextEdit.
+         */
+
         final CheckBox checkBoxC, checkBoxD;
         final EditText editTextA, editTextB, editTextC, editTextD;
 
@@ -80,44 +86,52 @@ public class MainActivity extends Activity {
         else if (id == R.id.action_version) { // TODO modify the version Toast to include the current version number. Then, modify it to bring up a custom 'About' toast/page.
             Toast.makeText(getApplicationContext(),R.string.toast_WIP,Toast.LENGTH_SHORT).show();
         }
-        else if (id == R.id.action_generate) { // TODO Fix the case where generation runs twice.
+        else if (id == R.id.action_generate) {
+        // FIXME Fix the case where generation runs twice.
+
             OptionPicker gen = new OptionPicker();
             final EditText a = (EditText) findViewById(R.id.editText_optionA),
                            b = (EditText) findViewById(R.id.editText_optionB),
                            c = (EditText) findViewById(R.id.editText_optionC),
                            d = (EditText) findViewById(R.id.editText_optionD);
+            final CheckBox checkC = (CheckBox) findViewById(R.id.checkBox_optionC),
+                           checkD = (CheckBox) findViewById(R.id.checkBox_optionD);
+            String result;
+            int x = 2, i;
+            long resultSleep = 5 * 1000;
 
             // TODO Dynamically add however many options we have to default/blank notification checker.
-            // TODO Add in the case to deny generation when an option field is enabled, but blank.
+            // FIXME Keep option d from running If c isn't enabled.
             if (a.getText().toString().contentEquals(getString(R.string.editText_blank)) || a.getText().toString().isEmpty()) Toast.makeText(getApplicationContext(),"Please enter Option A",Toast.LENGTH_SHORT).show();
             else if (b.getText().toString().contentEquals(getString(R.string.editText_blank)) || b.getText().toString().isEmpty()) Toast.makeText(getApplicationContext(),"Please enter Option B",Toast.LENGTH_SHORT).show();
             else if (c.isEnabled() && (c.getText().toString().isEmpty() || c.getText().toString().contentEquals(getString(R.string.editText_blank)))) Toast.makeText(getApplicationContext(),"Please enter Option C",Toast.LENGTH_SHORT).show();
             else if (d.isEnabled() && (d.getText().toString().isEmpty() || d.getText().toString().contentEquals(getString(R.string.editText_blank)))) Toast.makeText(getApplicationContext(),"Please enter Option D",Toast.LENGTH_SHORT).show();
             //else Toast.makeText(getApplicationContext(),R.string.toast_error + "in default/empty check",Toast.LENGTH_SHORT).show();
 
-            String result;
-            int x = 2;
-            final CheckBox checkC, checkD;
-            checkC = (CheckBox) findViewById(R.id.checkBox_optionC);
-            checkD = (CheckBox) findViewById(R.id.checkBox_optionD);
             if (checkC.isChecked()) {
                 x += 1;
                 if (checkD.isChecked()) x += 1;
             }
-            int i = gen.choose(x);
+
+            i = gen.choose(x);
             switch (i) {
                 case 1:
-                    result =  a.getText().toString();
-                    Toast.makeText(getApplicationContext(),result + "is the result, duh.", Toast.LENGTH_LONG);
+                    result = a.getText().toString();
+                    a.selectAll();
+                    //Thread.sleep(resultSleep); // FIXME Select answer, sleep/delay, then deselect and continue.
+                    Toast.makeText(getApplicationContext(),result + " is the result, duh. Obviously Testing...", Toast.LENGTH_LONG).show();
                     break;
                 case 2:
                     result = b.getText().toString();
+                    b.selectAll();
                     break;
                 case 3:
                     result = c.getText().toString();
+                     c.selectAll();
                     break;
                 case 4:
-                    result = b.getText().toString(); // TODO Fix this option switch to include all options.
+                    result = d.getText().toString();
+                    d.selectAll();
                     break;
                 default:
                     result = R.string.toast_error + " in SWITCH";
