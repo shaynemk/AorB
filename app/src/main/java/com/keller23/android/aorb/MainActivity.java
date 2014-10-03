2a9c1eb1cb8c37b821ed5ab3e1a1288efa232c42
shaynemk <uskickbutt@gmail.com>
2014-10-03 04:46:06 -0400

    Fixed the toggle of oC/D check/text boxes.However, if oD is enabled and oC isn't, it still generates. It also still generates twice per click. Last build/commit for tonight.

--- app/src/main/java/com/keller23/android/aorb/MainActivity.java
+++ app/src/main/java/com/keller23/android/aorb/MainActivity.java
@@ -52,6 +52,13 @@ public class MainActivity extends Activity {
                 if (editTextC.getText().toString().contentEquals(getString(R.string.editText_blank))) editTextC.setText("");
             }
         });
+        checkBoxC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkBoxC.isChecked()) editTextC.setEnabled(true);
+                else if (!checkBoxC.isChecked()) editTextC.setEnabled(false);
+            }
+        });
 
         checkBoxD = (CheckBox) findViewById(R.id.checkBox_optionD);
         editTextD = (EditText) findViewById(R.id.editText_optionD);
@@ -63,6 +70,14 @@ public class MainActivity extends Activity {
                 if (editTextD.getText().toString().contentEquals(getString(R.string.editText_blank))) editTextD.setText("");
             }
         });
+        checkBoxD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkBoxD.isChecked()) editTextD.setEnabled(true);
+                else if (!checkBoxD.isChecked()) editTextD.setEnabled(false);
+            }
+        });
+
     }
 
 
@@ -97,47 +112,50 @@ public class MainActivity extends Activity {
             final CheckBox checkC = (CheckBox) findViewById(R.id.checkBox_optionC),
                            checkD = (CheckBox) findViewById(R.id.checkBox_optionD);
             String result;
-            int x = 2, i;
-            long resultSleep = 5 * 1000;
+            int x = 2;
+            //long resultSleep = 5 * 1000;
 
             // TODO Dynamically add however many options we have to default/blank notification checker.
             // FIXME Keep option d from running If c isn't enabled.
+            // TODO Nest the oC and of if statements to require being selected as well as not empty/default.
             if (a.getText().toString().contentEquals(getString(R.string.editText_blank)) || a.getText().toString().isEmpty()) Toast.makeText(getApplicationContext(),"Please enter Option A",Toast.LENGTH_SHORT).show();
             else if (b.getText().toString().contentEquals(getString(R.string.editText_blank)) || b.getText().toString().isEmpty()) Toast.makeText(getApplicationContext(),"Please enter Option B",Toast.LENGTH_SHORT).show();
             else if (c.isEnabled() && (c.getText().toString().isEmpty() || c.getText().toString().contentEquals(getString(R.string.editText_blank)))) Toast.makeText(getApplicationContext(),"Please enter Option C",Toast.LENGTH_SHORT).show();
             else if (d.isEnabled() && (d.getText().toString().isEmpty() || d.getText().toString().contentEquals(getString(R.string.editText_blank)))) Toast.makeText(getApplicationContext(),"Please enter Option D",Toast.LENGTH_SHORT).show();
-            //else Toast.makeText(getApplicationContext(),R.string.toast_error + "in default/empty check",Toast.LENGTH_SHORT).show();
+            else { // FIXME If you select option D it will still generate regardless of oC being selected or oD being selected/not filled.
 
-            if (checkC.isChecked()) {
-                x += 1;
-                if (checkD.isChecked()) x += 1;
-            }
 
-            i = gen.choose(x);
-            switch (i) {
-                case 1:
-                    result = a.getText().toString();
-                    a.selectAll();
-                    //Thread.sleep(resultSleep); // FIXME Select answer, sleep/delay, then deselect and continue.
-                    Toast.makeText(getApplicationContext(),result + " is the result, duh. Obviously Testing...", Toast.LENGTH_LONG).show();
-                    break;
-                case 2:
-                    result = b.getText().toString();
-                    b.selectAll();
-                    break;
-                case 3:
-                    result = c.getText().toString();
-                     c.selectAll();
-                    break;
-                case 4:
-                    result = d.getText().toString();
-                    d.selectAll();
-                    break;
-                default:
-                    result = R.string.toast_error + " in SWITCH";
+                if (checkC.isChecked()) {
+                    x += 1;
+                    if (checkD.isChecked()) x += 1;
+                }
+
+                switch (gen.choose(x)) {
+                    case 1:
+                        result = a.getText().toString();
+                        a.selectAll();
+                        //Thread.sleep(resultSleep); // FIXME Select answer, sleep/delay, then deselect and continue.
+                        Toast.makeText(getApplicationContext(), result + " is the result, duh. Obviously Testing...", Toast.LENGTH_LONG).show();
+                        break;
+                    case 2:
+                        result = b.getText().toString();
+                        b.selectAll();
+                        break;
+                    case 3:
+                        result = c.getText().toString();
+                        c.selectAll();
+                        break;
+                    case 4:
+                        result = d.getText().toString();
+                        d.selectAll();
+                        break;
+                    default:
+                        result = R.string.toast_error + " in SWITCH";
+                }
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                //result = null; // TODO Figure out if result = null is even necessary, let alone helpful.
+                //return true; // TODO  Does this ake more sense (with below as false) or leave the true return below?
             }
-            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
-            //result = null; // TODO Figure out if result = null is even necessary, let alone helpful.
             return true;
         }
         return super.onOptionsItemSelected(item);
